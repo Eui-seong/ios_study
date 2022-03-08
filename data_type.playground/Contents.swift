65fdca461aa9f68fb653c117573e4dcc719bd4fe
Sineuiseong <tlsdmltjd52@gmail.com>
2022-03-09 02:01:59 +0900

    swift data type

---
+++ data_type.playground/Contents.swift
@@ -0,0 +1,23 @@
+import UIKit
+
+// Swift의 기본 데이터 타입
+// Bool, Int, UInt, Float, Double, Character, String
+
+// Bool
+var someBool: Bool = true
+someBool = false
+// someBool = 0
+// someBool = 1
+
+// Int
+var someInt: Int = -100
+
+// UInt
+// 양의 정수 타입
+var someUInt: UInt = 100
+//someUInt = someInt Error  
+// someUInt = -100 Error
+
+// Float
+var someFloat: Float = 3.14
+someFloat = 3
